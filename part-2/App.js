dcba2a5a93fe4e6e9a9d20e4a06ffaecfb845883
NickVendetti <nrvendetti@gmail.com>
2024-11-30 23:11:13 -0500

    Part 2/no CSS

---
+++ part-2/App.js
@@ -0,0 +1,7 @@
+const App = () => (
+    <div>
+        <TweetComponent UserName="NVendetti" DateTweet="12/12/2024" NameOfUser="Nick Vendetti" TweetMessage="I hope this works!"/>
+    </div>
+)
+
+ReactDOM.render(<App/>,document.getElementById("root"))
---
+++ part-2/TweetComponent.js
@@ -0,0 +1,8 @@
+const TweetComponent = (props) => (
+    <ul>
+      <li>User Name: {props.UserName}</li>
+      <li>Date: {props.DateTweet}</li>
+      <li>Name of User: {props.NameOfUser}</li>
+      <li>Tweet: {props.TweetMessage}</li>
+     </ul> 
+)
