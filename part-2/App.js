59406715a3254f791b12087ee43b73738daa84ef
NickVendetti <nrvendetti@gmail.com>
2024-12-01 18:39:57 -0500

    Part 2 complete

--- part-2/App.js
+++ part-2/App.js
@@ -1,6 +1,8 @@
 const App = () => (
     <div>
         <TweetComponent UserName="NVendetti" DateTweet="12/12/2024" NameOfUser="Nick Vendetti" TweetMessage="I hope this works!"/>
+        <TweetComponent UserName="SSCHMIDT" DateTweet="10/25/2022" NameOfUser="Summer Schmidt" TweetMessage="You are doing great!"/>
+        <TweetComponent UserName="MilesPerHR" DateTweet="05/04/1989" NameOfUser="Miles Angelo" TweetMessage="Are you learning anything?"/>
     </div>
 )
 
--- part-2/TweetComponent.js
+++ part-2/TweetComponent.js
@@ -1,8 +1,11 @@
 const TweetComponent = (props) => (
-    <ul>
-      <li>User Name: {props.UserName}</li>
-      <li>Date: {props.DateTweet}</li>
-      <li>Name of User: {props.NameOfUser}</li>
-      <li>Tweet: {props.TweetMessage}</li>
-     </ul> 
+    <div>
+      <ul>
+        <li>User Name: {props.UserName}</li>
+        <li>Date: {props.DateTweet}</li>
+        <li>Name of User: {props.NameOfUser}</li>
+        <li>Tweet: {props.TweetMessage}</li>
+      </ul> 
+    </div> 
+    
 )
